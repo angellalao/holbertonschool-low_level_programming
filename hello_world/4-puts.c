2ff2adff832cd40e38a8a82274d2a61251233373
angellalao <6369@holbertonstudents.com>
2023-02-17 05:39:30 -0800

    use puts to print string

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/**
- * add comment here later 
- */
+/*add comment here later*/
 int main(void)
 {
+	/*function description */
 	puts("\"Programming is like building a multilingual puzzle");
+	/*return descrption*/
 	return (0);
 }
