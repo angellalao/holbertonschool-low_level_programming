e5eedf9e6f4a353d5916817e9e4d6dd9fbeac302
angellalao <6369@holbertonstudents.com>
2023-02-17 17:36:58 -0800

    print size of various types on c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,9 +13,9 @@ int main()
 
 	/*Outputs size of each integer*/
 	printf("Size of integer: %d bytes\n", sizeof(inttype));
-	printf("Size of float: %f bytes\n", sizeof(floattype));
-	printf("Size of double float: %f bytes/n", sizeof(doubletype));
-	printf("Size of char: %c bytes/n", sizeof(chartype));
+	printf("Size of float: %d bytes\n", sizeof(floattype));
+	printf("Size of double float: %d bytes\n", sizeof(doubletype));
+	printf("Size of char: %d bytes/n", sizeof(chartype));
 
 	return (0);
 }
