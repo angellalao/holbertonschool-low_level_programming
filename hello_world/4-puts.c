04a712a7f3912fb4c26be973b895d1b877443d54
angellalao <6369@holbertonstudents.com>
2023-02-17 16:42:27 -0800

    print string followed by new line in c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /*
- * Function:
- * Return:
+ * main - this is the function description
+ *
+ * Return: - return header tag is mandatory if function returns
+ * any value but void
  */
 int main(void)
 {
-	/* Function description */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
