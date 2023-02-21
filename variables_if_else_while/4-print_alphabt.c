03fbd261fc73f2e9105b4831894a5702fb18cd1d
angellalao <6369@holbertonstudents.com>
2023-02-20 22:02:41 -0800

    print alphabet except qe

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+ * main - print alphabet except 'q' and 'e'
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char alph = 'a';
+
+	while (alph <= 'z' && alph != 'q' && alph != 'e')
+	{
+	putchar(alph);
+	alph++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
