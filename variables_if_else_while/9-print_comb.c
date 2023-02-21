0efbd9d882cf73c048e1b76c6ea9fe75bef2d287
angellalao <6369@holbertonstudents.com>
2023-02-20 23:38:40 -0800

    print single digit combos with putchar

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print all combinations of single digit numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		putchar(' ');
+		putchar(',');
+		putchar(' ');
+		num++;
+	}
+
+	return (0);
+}
