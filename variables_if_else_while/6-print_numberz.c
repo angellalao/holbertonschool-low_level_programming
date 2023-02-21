6399ef7d6a5e68348378851cacc40a74ae0f6c1a
angellalao <6369@holbertonstudents.com>
2023-02-20 23:09:37 -0800

    print numbers using putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+ * main - print numbers using putchar
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num);
+		num ++;
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
