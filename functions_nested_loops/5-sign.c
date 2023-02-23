9b9a7e5335ac7c5cfc08cdd9886f1290082c69d1
angellalao <6369@holbertonstudents.com>
2023-02-23 03:06:48 -0800

    prints sign of number

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -3,24 +3,24 @@
 /**
  * print_sign - checks if number is positive/negative or 0.
  * @n: number to be checked
- * Return: 0 if positive or 0, 1 if negative
+ * Return: 1 if positive, 0 if 0, 1 if negative
  */
 
 int print_sign(int n)
 {
-	if (n > '0')
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n < '0' && n != '0')
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
 	}
 	else
 	{
-		_putchar('0');
+		_putchar(0);
 		return (0);
 	}
 
