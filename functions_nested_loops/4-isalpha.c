7ba87d54e1fe388c7cc8223af01cae3568de2a23
angellalao <6369@holbertonstudents.com>
2023-02-22 17:56:57 -0800

    check for alphabet

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - return 1 if upper or lowercase letter
+ * @c: character to be tested
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		return ('1');
+	}
+	else
+	{
+		return ('0');
+	}
+
+}
