53aa7a1cada97c33ed6d6d210b6bda75ffcde830
angellalao <6369@holbertonstudents.com>
2023-02-24 04:51:57 -0800

    fizz buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints numbers 0-100, words replace numbers that are multiples
+ * of 3, 5 and 15
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	num = 1;
+	while (num <= 100)
+	{
+		if (((num % 3) == 0) && ((num % 5) != 0))
+		{
+			printf("Fizz");
+		}
+		else if (((num % 5) == 0) && ((num % 3) != 0))
+		{
+			printf("Buzz");
+		}
+		else if (((num % 3) == 0) && ((num % 5) == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", num);
+		}
+		printf(" ");
+		num = num + 1;
+	}
+	printf("\n");
+	return (0);
+}
