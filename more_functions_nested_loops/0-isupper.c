3553abbc3a45ceba9de6f64c56ce6557db5091c9
angellalao <6369@holbertonstudents.com>
2023-02-23 22:31:45 -0800

    check for uppercase

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - return 1 if uppercase, 0 if lowercase
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
