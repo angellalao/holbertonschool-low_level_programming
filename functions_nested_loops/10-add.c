61914324c7c4560b308d5327ea95c7825893c1ca
angellalao <6369@holbertonstudents.com>
2023-02-23 18:53:08 -0800

    addition of 2 intergers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds 2 integers
+ * @a: first number to be added
+ * @b: second number to be added
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
