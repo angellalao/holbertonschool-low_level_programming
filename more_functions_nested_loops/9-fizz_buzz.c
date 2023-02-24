805ffb76d3ec14ad8f985ea6ce1660f88c2c5f25
angellalao <6369@holbertonstudents.com>
2023-02-24 04:56:25 -0800

    fizz buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,21 +15,20 @@ int main(void)
 	{
 		if (((num % 3) == 0) && ((num % 5) != 0))
 		{
-			printf("Fizz");
+			printf("Fizz, ");
 		}
 		else if (((num % 5) == 0) && ((num % 3) != 0))
 		{
-			printf("Buzz");
+			printf("Buzz, ");
 		}
 		else if (((num % 3) == 0) && ((num % 5) == 0))
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz, ");
 		}
 		else
 		{
-			printf("%d", num);
+			printf("%d, ", num);
 		}
-		printf(" ");
 		num = num + 1;
 	}
 	printf("\n");
