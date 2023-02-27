f59dd31e7252d0969ef9493194ae8cbcf973ec46
angellalao <6369@holbertonstudents.com>
2023-02-26 21:38:24 -0800

    print string in reverse, followed by newline

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,34 @@
 #include "main.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string to be checked
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c = c + 1;
+
+	return (c);
+}
+
+/**
  * print_rev - print string in reverse, followed by newline
  * @s: string
  */
 void print_rev(char *s)
 {
 	int len;
-	int c;
 
 	len = _strlen(s);
-	c = 0;
 
-	while (s[c] != '\0')
+	while (len >= 0)
 	{
 		_putchar(s[len]);
 		len = len - 1;
-		c = c + 1;
 	}
 	_putchar('\n');
 }
