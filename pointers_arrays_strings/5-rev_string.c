5bcc8fe691bd03e2b6d2c3f9d85347740a37b80d
angellalao <6369@holbertonstudents.com>
2023-02-26 22:01:51 -0800

    reverse a string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be checked
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c = c + 1;
+
+	return (c);
+}
+
+/**
+ * rev_string - print string in reverse
+ * @s: string
+ */
+void rev_string(char *s)
+{
+	int len;
+
+	len = _strlen(s) - 1;
+
+	while (len >= 0)
+	{
+		_putchar(s[len]);
+		len = len - 1;
+	}
+}
