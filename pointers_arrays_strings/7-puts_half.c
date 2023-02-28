c9b70ec6314d95be4a5add92a79aec022e622636
angellalao <6369@holbertonstudents.com>
2023-02-28 01:13:17 -0800

    set count = 0, fixes segmentation fault

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be checked
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c = c + 1;
+
+	return (c);
+}
+
+/**
+ * puts_half - prints half a string, followed by newline
+ * @str: string to be inputted
+ */
+void puts_half(char *str)
+{
+	int count;
+	int len;
+	int num;
+
+	count = 0;
+
+	while (str[count] != '\0')
+	{
+		len = _strlen(str);
+		num = (len % 2);
+		if (num == 0)
+		{
+			count = (len / 2);
+			_putchar(str[count]);
+			while (count <= len)
+			{
+				count = count + 1;
+				_putchar(str[count]);
+			}
+		}
+		else if (num == 1)
+		{
+			count = ((len - 1) / 2);
+			_putchar(str[count]);
+			while (count <= len)
+			{
+				count = count + 1;
+				_putchar(str[count]);
+			}
+		}
+		_putchar('\n');
+	}
+}
