27d45eb3b882c130da8583bcbf3936a9f759ccbe
angellalao <6369@holbertonstudents.com>
2023-03-02 03:43:00 -0800

    pointers

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string to be checked
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c = c + 1;
+
+	return (c);
+}
+
+/**
  * *_strcpy - copies string pointed to by src, to the buffer
  * pointed to by dest
  * @dest: string copied to here
@@ -10,13 +25,19 @@
 char *_strcpy(char *dest, char *src)
 {
 	int c;
+	int len;
 
+	len = _strlen(src);
 	c = 0;
-	while (src[c] <= '\0')
+	while (c <= len)
 	{
-		*dest = *src;
-		src = src + 1;
-		dest = dest + 1;
+		dest[c] = src[c];
+		c = c + 1;
+
+	}
+	if (*src == '\0')
+	{
+		*dest = '\0';
 	}
 	return (dest);
 }
