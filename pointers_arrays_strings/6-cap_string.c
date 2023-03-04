b86b38196a8867a5656e63efdf4ad5ceabb3fafd
angellalao <6369@holbertonstudents.com>
2023-03-04 03:45:48 -0800

    capitalise all words

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *cap_string - capitalises all words of a string
+ * @str: string
+ * Return:str
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ')
+		{
+			i = i + 1;
+			if (str[i] <= 97 && str[i] >= 122)
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+		i = i + 1;
+	}
+	return (str);
+}
