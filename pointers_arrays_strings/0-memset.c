7c34ad1939851970904d762fb1233eaa349637dc
angellalao <6369@holbertonstudents.com>
2023-03-04 05:47:32 -0800

    fills memory with constant byte

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -12,13 +12,10 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned int i;
 
 	i = 0;
-	while (s[i] != '\0')
+	while (i < n)
 	{
-		while (i < n)
-		{
-			s[i] = b;
-			i = i + 1;
-		}
+		s[i] = b;
+		i = i + 1;
 	}
 	return (s);
 }
