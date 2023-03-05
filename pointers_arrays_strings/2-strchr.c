b3fd8a1ca53329da35c24f083e91dc80b58aaf44
angellalao <6369@holbertonstudents.com>
2023-03-04 21:02:36 -0800

    locate character in string

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * *_strchr - locates a character in a string
@@ -8,8 +9,8 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-	char *ptr = '\0';
+	int i;
+	char *ptr = NULL;
 
 	i = 0;
 	while (s[i] != '\0')
