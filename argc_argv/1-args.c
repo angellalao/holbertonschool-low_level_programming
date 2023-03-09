4588aa50997bea68052bd94c2b8168394d346818
angellalao <6369@holbertonstudents.com>
2023-03-09 00:10:41 -0800

    unused attribute added

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -7,11 +7,8 @@
  * @argv: arguments
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, __attribute__ ((unused)) char *argv[])
 {
-	if (argv[0])
-	{
-		printf("%d\n", argc - 1);
-	}
+	printf("%d\n", argc - 1);
 	return (0);
 }
