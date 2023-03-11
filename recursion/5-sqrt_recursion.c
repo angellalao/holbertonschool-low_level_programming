2df448c538cdd5e3be5abde6a3553528a345e050
angellalao <6369@holbertonstudents.com>
2023-03-10 19:30:48 -0800

    natural square root

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * sq_root - helper function to find natural square root of number
+ * @n: number
+ * @root: starting number to find root
+ * Return: square root or -1 if square root doesnt exist
+ */
+
+int sq_root(int n, int root)
+{
+	if (root * root == n)
+	{
+		return (root);
+	}
+	else if (root < 0)
+	{
+		return (-1);
+	}
+	return (sq_root(n, root - 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of number
+ * @n: number
+ * Return: square root or -1 if square root doesnt exist
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (sq_root(n, n));
+}
