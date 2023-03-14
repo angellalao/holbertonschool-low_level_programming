935c0fe0d140e47b427b2ffd975d2390e9c2edb9
angellalao <6369@holbertonstudents.com>
2023-03-14 03:32:46 -0700

    add NULL statement

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -41,12 +41,20 @@ char *str_concat(char *s1, char *s2)
 	i = 0;
 	while (s1[i] != '\0')
 	{
+		if (s1 == NULL)
+		{
+			break;
+		}
 		newstring[i] = s1[i];
 		i = i + 1;
 	}
 	j = 0;
 	while (s2[j] != '\0')
 	{
+		if (s2 == NULL)
+		{
+			break;
+		}
 		newstring[i] = s2[j];
 		i = i + 1;
 		j = j + 1;
