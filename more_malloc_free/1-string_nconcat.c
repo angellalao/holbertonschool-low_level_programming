0f6602689840b4961e3ff98f1a8d9a8f7a167359
angellalao <6369@holbertonstudents.com>
2023-03-16 01:26:51 -0700

    return empty string if NULL is passed

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	}
 
-	ptr = malloc(sizeof(s1) + n);
+	ptr = malloc(sizeof(s1) + (sizeof(char) *n));
 	if (ptr == NULL)
 	{
 		return (NULL);
