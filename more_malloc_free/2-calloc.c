24b5d108ffe570aabbaaf45c2611767db8152126
angellalao <6369@holbertonstudents.com>
2023-03-16 18:25:12 -0700

    allocate memory for an array using malloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *_calloc - Write a function that allocates memory for an array, using malloc
+ * @nmemb: number of elements
+ * @size: size in bytes of each element
+ * Return:
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	unsigned int *array;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	array = malloc(sizeof(size) * nmemb);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < nmemb)
+	{
+		array[i] = 0;
+		i = i + 1;
+	}
+	return (array);
+}
