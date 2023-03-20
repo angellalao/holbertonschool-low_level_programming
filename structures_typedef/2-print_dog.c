c87d910a9aec424afecbe40ad5c856cc69d1b827
angellalao <6369@holbertonstudents.com>
2023-03-20 06:01:45 -0700

    print struct dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog -
+ * @d:
+ */
+void print_dog(struct dog *d)
+{
+	d = malloc((sizeof(struct dog)));
+	if (d == NULL)
+	{
+		exit(1);
+	}
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	if (d->age == 0)
+	{
+		printf("Age: (nil)\n");
+	}
+	else
+	{
+		printf("Age: %f\n", d->age);
+	}
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+	free(d);
+}
