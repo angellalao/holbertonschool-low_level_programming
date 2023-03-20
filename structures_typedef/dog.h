bc25051adc2197ed9ae18def8fe6c79080dc093f
angellalao <6369@holbertonstudents.com>
2023-03-19 18:26:07 -0700

    add guards

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * dog - define new struct called dog
+ */
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
 	char *owner;
 };
+
+#endif /* #ifndef _DOG_H_ */
