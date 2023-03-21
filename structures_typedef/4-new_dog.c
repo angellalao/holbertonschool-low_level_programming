73eb872734050624b852441de3af528a89eeb6a7
angellalao <6369@holbertonstudents.com>
2023-03-21 16:35:09 -0700

    change malloc

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,42 @@
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include "dog.h"
+
+/**
+ * *_strdup - function that returns a pointer to a newly allocated space
+ * in memory, which contains a copy of the string given as a parameter
+ * @str: string to be copied
+ * Return: NULL or pointer to copied string
+ */
+char *_strdup(char *str)
+{
+	char *newstring;
+	int i;
+	int j;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i = i + 1;
+	}
+	newstring = malloc(sizeof(char) * (i + 1));
+	if (newstring == NULL)
+	{
+		return (NULL);
+	}
+	j = 0;
+	while (j <= i)
+	{
+		newstring[j] = str[j];
+		j = j + 1;
+	}
+	return (newstring);
+}
 
 /**
  * *new_dog - creates a new dog
@@ -11,15 +47,30 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *dog_t;
+	dog_t *p;
+	char *namecopy;
+	char *ownercopy;
 
-	dog_t = malloc(sizeof(struct dog));
-	if (dog_t == NULL)
+	p = malloc(sizeof(*p));
+	if (p == NULL)
 	{
 		return (NULL);
 	}
-	dog_t->name = name;
-	dog_t->age = age;
-	dog_t->owner = owner;
-	return (dog_t);
+	namecopy = _strdup(name);
+	if (namecopy == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	p->name = namecopy;
+	p->age = age;
+	ownercopy = _strdup(owner);
+	if (ownercopy == NULL)
+	{
+		free(namecopy);
+		free(p);
+		return (NULL);
+	}
+	p->owner = ownercopy;
+	return (p);
 }
