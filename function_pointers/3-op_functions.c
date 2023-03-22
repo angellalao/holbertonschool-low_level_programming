3780f39d445f67fc03e5ef5bbe162fe5672adcc6
angellalao <6369@holbertonstudents.com>
2023-03-22 01:10:02 -0700

    add descriptions

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -2,19 +2,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * op_add - returns sum of a and b
+ * @a: first number
+ * @b: second number
+ * Return: sum (int)
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
+/**
+ * op_sub - subtracts b from a
+ * @a: number to be subtracted from
+ * @b: number to be subtracted
+ * Return: (int) value of b subtracted from a
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
+/**
+ * op_mul - a multiplied by b
+ * @a: first number to be multiplied
+ * @b: second number to be multiplied
+ * Return: (int) product of a and b
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
+/**
+ * op_div - returns result of a divided by b
+ * @a: number to by divided
+ * @b: number to be divided by
+ * Return: (int) result of division of a by b
+ */
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -24,6 +50,13 @@ int op_div(int a, int b)
 	}
 	return (a / b);
 }
+
+/**
+ * op_mod - returns remainder of the division of a by b
+ * @a: number to be divided
+ * @b: number to divide by
+ * Return: (int) remainder of the division
+ */
 int op_mod(int a, int b)
 {
 	if (b == 0)
