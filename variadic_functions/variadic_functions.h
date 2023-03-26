711f84ae54a44c64575a42a6c4edb54feeec612c
angellalao <6369@holbertonstudents.com>
2023-03-25 23:42:23 -0700

    update for q3

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,24 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
 
+#include <stdarg.h>
+
+/**
+ * struct type - struct used to match operator with correlating function
+ * @op: string representation of the operator
+ * @f: function pointer for the operation to be used
+ */
+typedef struct type
+{
+	char *op;
+	void (*f)(va_list ptr);
+} type;
+
+void print_int(va_list ptr);
+void print_char(va_list ptr);
+void print_str(va_list ptr);
+void print_float(va_list ptr);
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
