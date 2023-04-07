309133feaf2b0538ef2bff024165c2f960265710
angellalao <6369@holbertonstudents.com>
2023-04-06 19:30:11 -0700

    fixed description for betty

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -2,7 +2,7 @@
 
 /**
  * free_dlistint - frees/ releases the memory allocated for a list
- * head: pointer to the first node of the list to free
+ * @head: pointer to the first node of the list to free
  */
 void free_dlistint(dlistint_t *head)
 {
