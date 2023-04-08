f37197b19eacaed15a861d0d2cde742a3965336b
angellalao <6369@holbertonstudents.com>
2023-04-08 05:53:07 -0700

    added condition for index > range of bits in n

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -11,15 +11,15 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i;
 
+	if (index > (sizeof(n) * 8))
+	{
+		return (-1);
+	}
 	i = 0;
 	while (i < index)
 	{
 		n = n >> 1;
 		i = i + 1;
-		if (n == 0)
-		{
-			return (-1);
-		}
 	}
 	return (n & 1);
 }
