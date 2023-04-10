3fb8588fa32b77629a8b5510786e9768aa86ce6f
angellalao <6369@holbertonstudents.com>
2023-04-09 23:57:48 -0700

    changed permissions

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,17 +8,15 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int fd2;
-	ssize_t bytes_read;
-	ssize_t bytes_written;
+	int fd, fd2;
+	ssize_t bytes_read, bytes_written;
 	char buffer[1024];
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT, S_IRUSR | S_IWUSR | O_TRUNC);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
 		return (-1);
