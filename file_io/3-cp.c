2c24abc07d179dc5a568abc89422b7b8690ce6f0
angellalao <6369@holbertonstudents.com>
2023-04-15 21:23:43 -0700

    changed permissions

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -58,7 +58,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fdto = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	fdto = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fdto == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
